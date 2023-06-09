dc4f51bf1cfe4a615981874b233c4370b86da740
Kornrawee Kochtat <85238278+oransom48@users.noreply.github.com>
2023-06-10 03:36:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,18 @@ This app predicts your disease.
 """)
 
 @st.cache_resource
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+def encoder_txt():
+  return hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
+embed = encoder_txt()
 symptom = st.text_input('What Happened?',"I have no energy and have lost my appetite. I'm feeling really sick and don't know what's wrong.")
 ask_embed = embed([symptom])
 
 @st.cache_resource
 def load_model():
   return joblib.load('LinearSVC1_model.joblib')
-model = load_model()
 
+model = load_model()
 disease = model.predict(ask_embed)
 answer = disease[0]
 
